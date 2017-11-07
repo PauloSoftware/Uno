f84c7e2d99950474f2d2dc7b8affdeba0ff54192
Kayaite <32520979+Kayaite@users.noreply.github.com>
2017-11-07 13:50:05 +0100

    Grosse up

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-#liste des flag pour pas en avoir 2 les memes : flag1 flag2 flag3 flag4 flag5 flag6
+#liste des flag pour pas en avoir 2 les memes : flag1 flag2 flag3 flag4 flag5 flag6 flag7 flag8 flag9
 from random import *
 liste_cartes = []
 pioche = []
@@ -11,18 +11,21 @@ tas = []
 liste_joueurs=[]
 flag5= 0
 playtimes=0
-def lancer ():
-    n_joueurs=int(input("combien de joueurs"))
-    flag5= 0
-    while n_joueurs>flag5 :
-        nompartiel=input("nom")
-        liste_totale.append([[nompartiel],[]])
-        flag5+=1
-def placement_possible (ID_joueur,ID_carte):
-    if (tas[0][0]==liste_totale[ID_joueur][1][ID_carte][0]) or (tas[0][1]==liste_totale[ID_joueur][1][ID_carte][1]) or (liste_totale[ID_joueur][1][ID_carte][1]=="noir"):
-        return True
-    else :
-        return False
+realplaytimes=0
+flag7=0
+p2=0
+p4=0
+sens=1
+                        ############################
+                        # On définit les fonctions #
+                        ############################
+def voir_tas ():
+    print ("un" , tas[0][0] , tas[0][1] , "est sur le haut du tas")
+    return tas[0]
+
+def placement_possible(ID_joueur,ID_carte):
+    return (tas[0][0]==liste_totale[ID_joueur][1][ID_carte][0]) or (tas[0][1]==liste_totale[ID_joueur][1][ID_carte][1]) or (liste_totale[ID_joueur][1][ID_carte][1]=="noir")
+
 def clean (b):
     a = 0
     while a<b :
@@ -41,11 +44,11 @@ def voir_main (ID):
     while flag4<len(liste_totale[ID][1]) :
         print (flag4 ,"-->", liste_totale[ID][1][flag4][0], liste_totale[ID][1][flag4][1])
         flag4 +=1
-def jouer (ID , index) :
+def jouer (ID_joueur , ID_carte) :
     if placement_possible(ID_joueur,ID_carte) :
         del tas[0]
         tas.append (liste_totale[ID_joueur][1][ID_carte])
-        del liste_totale[ID_joueur][1][ID_carte]
+        del (liste_totale[ID_joueur][1][ID_carte])
     else :
         print ("placement impossible")
         return False
@@ -54,6 +57,9 @@ def periscope ():
     while flag6<len(liste_totale) :
         print (str(liste_totale[flag6][0][0])+"-->"+ str(len(liste_totale[flag6][1])))
         flag6+=1
+                        ###################################
+                        # On lance le brassage des cartes #
+                        ###################################
 liste_couleurs = ["bleu","rouge","vert","jaune"]
 while couleur!=4 :
     #le 0 n'est présent qu'une fois par couleur, on va l'insérer
@@ -83,6 +89,77 @@ while len(liste_cartes)>1 :
     flag=randint(1 , (len(liste_cartes)-1))
     pioche.append(liste_cartes[flag])
     del liste_cartes[flag]
+                        ###############################
+                        # On fait piocher les joueurs #
+                        ###############################
+while periode == "pioche" :
+    n_joueurs=int(input("combien de joueurs"))
+    flag5= 0
+    while n_joueurs>flag5 :
+        nompartiel=input("nom")
+        liste_totale.append([[nompartiel],[]])
+        flag5+=1
+    tas.append(pioche[0])
+    del pioche[0]
+    while flag7<len(liste_totale):
+        piocher(flag7,7)
+        flag7+=1
+    periode = "jeu"
 
-#while not (main1==0)and (main2==0):
-    
+                        ###########################
+                        # On commence le playtime #
+                        ###########################
+while periode == "jeu":
+    ajoué=False
+    aposé=False
+    while ajoué==False:
+        ID_jeu=playtimes%len(liste_totale)
+        clean(50)
+        #annonce des plus 2/4 en suspens
+        if p2!=0 : print("Vous avez" , p2 , "plus 2 en suspens")
+        if p4!=0 : print("Vous avez" , p4 , "plus 4 en suspens")
+        voir_tas()
+        print("A votre tour :",liste_totale[ID_jeu][0][0])
+        voir_main(ID_jeu)
+        print ("tapez",len(liste_totale[ID_jeu][1]),"pour piocher")
+        action_primaire = int(input())
+        
+        if action_primaire<len(liste_totale[ID_jeu][1]):
+            action_secondaire=liste_totale[ID_jeu][1][action_primaire]
+            jouer(ID_jeu,action_primaire)
+            if jouer != False:
+                ajoué=True
+                aposé=True
+        elif action_primaire==len(liste_totale[ID_jeu][1]):
+            action_secondaire=tas[0]
+            piocher(ID_jeu,1)
+            ajoué=True
+        else :
+            input("commande invalide !")
+        #gestion des cartes noires
+        if tas[0][1]=="noir":
+            flag9=input("choisissez une couleur")
+            if flag9 in liste_couleurs :
+                tas[0][1]=flag9
+            else:
+                ajoué=False
+                aposé=False
+                input("saisie invalide !")
+    #gestion des plus 2
+    if (action_secondaire[0]=="plus 2") and aposé :
+        p2+=1
+    elif p2!=0:
+        piocher(ID_jeu,2*p2)
+        p2=0
+    #gestion des plus 4
+    if (action_secondaire[0]=="plus 4") and aposé:
+        p4+=1
+    elif p4!=0:
+        piocher(ID_jeu,4*p4)
+        p4=0
+    if (action_secondaire[0]=="change de sens") and aposé :
+        sens=sens*(-1)
+    if (action_secondaire[0]=="passe ton tour") and aposé :
+        playtimes+=sens
+    playtimes+=sens
+    realplaytimes+=1
